63ba9711b384fea91facd62683b74e65c548be19
VarshUI <maverickvarsh@gmail.com>
2025-08-04 23:03:01 +0200

    login css done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,19 +3,32 @@ import Header from './Header'
 
 const Login = () => {
   return (
-    <div>
-          <Header />
-        <div className='absolute'>       
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/SE-en-20250721-TRIFECTA-perspective_6433145a-f507-469a-bf31-29c659178044_small.jpg" alt="background" className="bg-gradient-to-b from-white" />
-        </div>
-        <form className='absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0'>
-        <input type="text" placeholder="EmailAddress" className='p-2 m-2 bg-amber-50' />
-        <input type="password" placeholder="Password" className='p-2 m-2 bg-amber-50' />
-        <button className='p-4 m-4 bg-red-600'>Sign In </button>
-
-
-    </form>
-       
+    <div className="relative h-screen w-screen bg-black">
+      <Header />
+      <div className="absolute inset-0">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/SE-en-20250721-TRIFECTA-perspective_6433145a-f507-469a-bf31-29c659178044_small.jpg"
+          alt="background"
+          className="h-full w-full object-cover opacity-50"
+        />
+        <div className="absolute inset-0 bg-black opacity-60"></div> 
+      </div>
+      <form className="absolute z-10 p-12 bg-black bg-opacity-75 w-3/12 my-36 mx-auto left-0 right-0 text-white">
+        <h2 className="font-bold text-3xl pb-6">Sign In</h2>
+        <input
+          type="text"
+          placeholder="Email or phone number"
+          className="w-full p-3 my-2 bg-[#333] text-white rounded"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-3 my-2 bg-[#333] text-white rounded"
+        />
+        <button className="w-full p-3 my-4 bg-red-600 hover:bg-red-700 rounded font-semibold">
+          Sign In
+        </button>
+      </form>
     </div>
   )
 }
